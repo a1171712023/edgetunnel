0a70c98c36c4fda07dfbfade227ac1cc8cc6dca4
a117171 <a1171712023@outlook.com>
2025-04-09 20:33:36 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '58c95ee1-1320-439f-b41b-083d4962fa59';
-let proxyIP = 'proxyip.cmliussss.net:443';
+let proxyIP = 'us.a117171.ip-ddns.com';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
